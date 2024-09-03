0d90447f5d6c2204af5ea6e32a81b6e216c712af
Ephrem <ephrem08matheus@gmail.com>
2024-09-03 19:31:34 -0300

    finalizado
    projeto finalizado com alteraçoes no css para que houvesse uma maior responsividade do site

---
+++ images/index.js
@@ -0,0 +1,54 @@
+const inputBox = document.querySelector('#input-caixa'); 
+const listContainer = document.querySelector('#lista-container')
+
+const btnAdc = document.querySelector('.btn-adc');
+
+function apagaInput(){
+    inputBox.value = '';
+    inputBox.focus();
+}
+
+
+btnAdc.addEventListener('click', function(){
+    if (!inputBox.value){
+        alert('Você precisa digitar algo.')
+    }
+    else{
+        let li = document.createElement('li');
+        txtValue = document.createTextNode(inputBox.value);
+        
+        li.appendChild(txtValue);
+        listContainer.appendChild(li);
+        apagaInput()
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        salvarInformacao()
+    }
+    
+
+})
+
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        salvarInformacao()
+    }
+    if (e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        salvarInformacao();
+    }
+}, false)
+
+function salvarInformacao(){
+    localStorage.setItem('tarefas', listContainer.innerHTML);
+}
+
+function mostraTarefa(){
+    const tarefasSalvas = localStorage.getItem('tarefas');
+    listContainer.innerHTML = tarefasSalvas || '';
+}
+
+mostraTarefa();
+
+
